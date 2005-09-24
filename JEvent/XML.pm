ddd32b7140139500cdb2cfad6c7594554d984cef
Leif Johansson <leifj@su.se>
2005-09-24 16:52:03 +0000

    XML::Simple-based event callbacks

---
+++ JEvent/XML.pm
@@ -0,0 +1,22 @@
+package JEvent::XML;
+
+use XML::Simple;
+
+sub xmlEventCB
+{
+   my ($self,$sid,$msg) = @_;
+ 
+   my $items = $msg->GetItems();
+   return unless $items;
+   foreach my $item ($items->GetItems()) {
+      my $xml = XMLin($item->GetContent(),ForceArray=>1);
+      &{$self->{XEventCB}}($self,$sid,$xml);
+   }
+}
+
+sub PreExecute
+  {
+    my $self = shift;
+
+    $self->{EventCB} = \&xmlEventCB if ref $self->{XEventCB} eq 'CODE';
+  }
